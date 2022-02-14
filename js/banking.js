b4f00f0496917effd59a83ad7cdaa586f58368a7
rahathossain3 <rahathossain210@gmail.com>
2022-02-14 09:38:59 +0600

    final

--- js/banking.js
+++ js/banking.js
@@ -17,7 +17,36 @@ function getInputValue(inputId) {
     inputField.value = '';
     return amountValue;
 }
+function updateTotalField(totalFieldId, amount) {
+    // debugger;
+    const totalElement = document.getElementById(totalFieldId);  // 3. get and update total
+    const totalText = totalElement.innerText        //get deposit total inner text
+    const previousTotal = parseFloat(totalText);    // convate number
+    totalElement.innerText = previousTotal + amount;     //link between input and deposit
+}
+function getCurrentBlance() {
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;  // balance inner text
+    const previousBalanceTotal = parseFloat(balanceTotalText);     // convate number
+    return previousBalanceTotal;
+}
 
+function updateBalance(amount, isAdd) {
+    const balanceTotal = document.getElementById('balance-total');
+    /*    
+    const balanceTotalText = balanceTotal.innerText;  // balance inner text
+      const previousBalanceTotal = parseFloat(balanceTotalText);     // convate number
+       */
+
+    const previousBalanceTotal = getCurrentBlance()
+
+    if (isAdd == true) {
+        balanceTotal.innerText = previousBalanceTotal + amount;    // link between balance & deposit input
+    }
+    else {
+        balanceTotal.innerText = previousBalanceTotal - amount;    // link between balance & deposit input
+    }
+}
 
 
 
@@ -31,29 +60,37 @@ document.getElementById('deposit-button').addEventListener('click', function () 
       // convate number-----------------
       const depositAmount = parseFloat(depositAmountText); */
     //---------------------------------
-    const depositAmount = getInputValue('deposit-input');
 
-    // 3. get and update deposit total
-    const depositTotal = document.getElementById('deposit-total');
-    //get deposit total inner text
-    const depositTotalText = depositTotal.innerText
-    // convate number
-    const previousDepositTotal = parseFloat(depositTotalText);
 
-    //link between input and deposit
-    depositTotal.innerText = previousDepositTotal + depositAmount;
+    /*  // 3. get and update deposit total
+     const depositTotal = document.getElementById('deposit-total');
+     //get deposit total inner text
+     const depositTotalText = depositTotal.innerText
+     // convate number
+     const previousDepositTotal = parseFloat(depositTotalText);
+ 
+     //link between input and deposit
+     depositTotal.innerText = previousDepositTotal + depositAmount; */
     //-----------------------------------
+
 
     //update balance
     //get balance
-    const balanceTotal = document.getElementById('balance-total');
-    // balance inner text
-    const balanceTotalText = balanceTotal.innerText;
-    // convate number
-    const previousBalanceTotal = parseFloat(balanceTotalText);
 
-    // link between balance & deposit input
-    balanceTotal.innerText = previousBalanceTotal + depositAmount;
+    /*     const balanceTotal = document.getElementById('balance-total');
+        // balance inner text
+        const balanceTotalText = balanceTotal.innerText;
+        // convate number
+        const previousBalanceTotal = parseFloat(balanceTotalText);
+    
+        // link between balance & deposit input
+        balanceTotal.innerText = previousBalanceTotal + depositAmount; */
+    const depositAmount = getInputValue('deposit-input');
+    if (depositAmount > 0) {
+        updateTotalField('deposit-total', depositAmount);
+        updateBalance(depositAmount, true);
+    }
+
     //-------------------------------------
 
 
@@ -68,26 +105,38 @@ document.getElementById('withdraw-button').addEventListener('click', function ()
         // input convate number----------------
         const withdrawAmount = parseFloat(withdrawAmountText); */
 
-    const withdrawAmount = getInputValue('withdraw-input');
 
-    // 3. get and update deposit total
 
-    //update withdraw total-------------------------------
+    // 3. get and update withdrawtotal
+
+
+    /* //update  total-------------------------------
     const withdrawTotal = document.getElementById('withdraw-total');
     //get withdraw balacne
     const previousWithdrawTotalText = withdrawTotal.innerText;
     // convate number
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalText);
     // get final withdraw
-    withdrawTotal.innerText = withdrawAmount + previousWithdrawTotal;
+    withdrawTotal.innerText = withdrawAmount + previousWithdrawTotal; */
 
-    //update balance after withdraw------------------------------------
-    const balanceTotal = document.getElementById('balance-total');
-    const balanceTotalText = balanceTotal.innerText;
-    const previousBalanceTotal = parseFloat(balanceTotalText);
-
-    balanceTotal.innerText = previousBalanceTotal - withdrawAmount;
 
 
 
+    //update balance after withdraw------------------------------------
+
+    /*    const balanceTotal = document.getElementById('balance-total');
+       const balanceTotalText = balanceTotal.innerText;
+       const previousBalanceTotal = parseFloat(balanceTotalText);
+   
+       balanceTotal.innerText = previousBalanceTotal - withdrawAmount; */
+    const withdrawAmount = getInputValue('withdraw-input');
+    const currentBlance = getCurrentBlance();
+
+    if (withdrawAmount > 0 && withdrawAmount < currentBlance) {
+        updateTotalField('withdraw-total', withdrawAmount);
+        updateBalance(withdrawAmount, false);
+    }
+    if (withdrawAmount < currentBlance) {
+        console.log('you can not withdraw more then what your account');
+    }
 });
